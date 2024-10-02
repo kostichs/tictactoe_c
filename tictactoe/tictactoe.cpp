1995226e115f505506e2bd8ddeded3a06dc195e5
sergey.kostichev <sergey.kostichev@gcore.com>
2024-10-02 18:37:13 +0500

    Created a board

--- tictactoe/tictactoe.cpp
+++ tictactoe/tictactoe.cpp
@@ -1,21 +1,19 @@
-// tictactoe.cpp : This file contains the 'main' function. Program execution begins and ends there.
-
-
 #include <iostream>
 
 using namespace std;
 
+
 const int b_size = 4;
 const int p_size = 2;
 
-void play_game(string board[b_size][b_size], string players[p_size]);
-
+void play_game(char board[][b_size], string players[p_size]);
+void draw_board(char board[][b_size]);
 
 
 int main()
 {
 
-    string board[b_size][b_size];
+    char board[b_size][b_size];
     string players[p_size];
 
 
@@ -24,14 +22,24 @@ int main()
     do {
 
         play_game(board, players);
+        draw_board(board);
+
+
 
 	} while (true);
 
+    return 0;
 }
 
-void draw_board()
+void draw_board(char board[b_size][b_size])
 {
+    for (int row = 0; row < b_size; row++) {
+        for (int col = 0; col < b_size; col++) {
+            cout << board[row][col] << ' ';
 
+        }
+        cout << endl;
+    }
 }
 
 void define_players()
@@ -50,8 +58,21 @@ void calculate_positions()
 
 }
 
-void play_game(string board[b_size][b_size], string players[p_size])
+void play_game(char board[b_size][b_size], string players[p_size])
 {
+    board[0][0] = ' ';
+    for (int i = 1; i < b_size; i++)
+    {
+        board[0][i] = i + '0';
+        board[i][0] = i + '0';
+    }
+
+    for (int row = 1; row < b_size; row++) {
+        for (int col = 1; col < b_size; col++) {
+            board[row][col] = '.';
+
+        }
+    }
 
     for (int i = 0; i < p_size; i++)
     {
