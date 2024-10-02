582c61e576449ab41bda452ca562895ccf3b85e8
sergey.kostichev <sergey.kostichev@gcore.com>
2024-10-02 16:50:50 +0500

    Implemented first structure

--- tictactoe/tictactoe.cpp
+++ tictactoe/tictactoe.cpp
@@ -1,20 +1,54 @@
 // tictactoe.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 
 #include <iostream>
 
+using namespace std;
+
+void play_game();
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello User!\n";
+
+    do {
+
+        play_game();
+
+	} while (true);
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void draw_board()
+{
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+}
+
+void define_players()
+{
+  
+
+}
+
+void turn_player()
+{
+
+}
+
+void calculate_positions()
+{
+
+}
+
+void play_game()
+{
+    string players[2];
+    for (int i = 0; i < 2; i++)
+    {
+        cout << "Who is the first Player: Bot or Human?" << endl;
+        cin >> players[i];
+    }
+
+    cout << players[0] << " is playing against " << players[1] << "! Fight!" << endl;
+}
